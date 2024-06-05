44608a24e91a3cf7087b9d8b3e7267faa3e9ef60
Jyoti KM <88364055+JyotiKM29@users.noreply.github.com>
2024-06-05 11:03:22 +0530

    Time: 3 ms (71.35%), Space: 12.2 MB (7.78%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -2,44 +2,23 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int x) {
         int n = nums.size();
-        bool isAsc = nums[n-1] > nums[0] ;
-
-        int start = 0;
-        int end = n-1;
-
-        // start case
-        if(x < nums[0]) return 0;
-        if( x > nums[end]) return n;
+        int low = 0 ;
+        int high = n-1 ;
+        int ans = n ;
         
-        while(start <= end){
-           int middle = start + (end-start)/2;
-
-           if(nums[middle] == x){
-            return middle;
-           }
-
-           if(isAsc){
-             if(nums[middle] < x){
-                start = middle + 1;
-             }else{
-                end = middle - 1; 
-             }
-           }else{
-             if(nums[middle] > x){
-                start = middle + 1;
-             }else{
-                end = middle - 1; 
-             }
-           }
-
-         
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            
+            if(nums[mid] >= x){
+                ans = mid ;
+                high = mid - 1;
+            
+            }else{
+                low = mid + 1;
+                
+            }
         }
-
-        if(x > nums[end]){
-            return start;
-        }else{
-            return end;
-        }
-          
+        
+        return ans ;
     }
 };
