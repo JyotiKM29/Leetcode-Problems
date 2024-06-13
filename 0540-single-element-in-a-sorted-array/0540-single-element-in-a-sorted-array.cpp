f477eaa1e6cad418938b942cca8f6c32f83d9d54
Jyoti KM <88364055+JyotiKM29@users.noreply.github.com>
2024-06-13 23:21:54 +0530

    Time: 22 ms (31.33%), Space: 24.5 MB (99.24%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-       int n = nums.size();
-        if(n == 1)  return nums[0];
-        for(int i = 0 ; i < n ; i++){
-            if(nums[0] != nums[1]) return nums[0];
-            else if(nums[n-1] != nums[n-2]) return nums[n-1];
+       //Binary Search way 
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        else if(nums[0] != nums[1]) return nums[0];
+        else if(nums[n-1] != nums[n-2]) return nums[n-1];
+        
+        int low = 1 ;
+        int high = n-2 ;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
             
-            else{
-                if(nums[i] != nums[i+1] && nums[i] !=nums[i-1]){
-                    return nums[i];
-                }
+            if(nums[mid] != nums[mid +1] && nums[mid]!=nums[mid-1]){
+                return nums[mid];
+            }
+            if((mid % 2 == 1 && nums[mid-1 ]== nums[mid])|| (mid %2 == 0 && nums[mid] == nums[mid+1])){
+                low = mid + 1;
+            }else{
+                high = mid -1;
             }
         }
+               return -1;
         
-        return -1;
+        
     }
 };
